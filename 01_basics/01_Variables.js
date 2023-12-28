a20f01d474ca611dd594a8527b9fc2fc86801f34
Suryakanth L Raibagi <114281722+SuryakanthRaibagi@users.noreply.github.com>
2023-12-28 20:45:34 +0000

    variable and datatype

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,19 @@
+const accountId =144553
+let accountEmail="helloworld@gmail.com"
+var accountPassword ="12345"
+accountCity ="Karnataka"
+
+
+//accountId=2
+
+accountEmail ="HC.com"
+accountPassword="12121"
+accountCity= "Bang"
+
+/*
+Prefer not to use Var 
+becouse of issue in block and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+// alert("Hello")//we are using node js not browser
+
+// console.log(3+3);
+
+
+let age=18
+let isLoggedIn=false
+let state;
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof undefined);//undefined 
+console.log(typeof null);//object
