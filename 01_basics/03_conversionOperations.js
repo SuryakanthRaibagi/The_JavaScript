53d33edb264f1c34be4a546a1c21f1065ec8e0d6
Suryakanth L Raibagi <114281722+SuryakanthRaibagi@users.noreply.github.com>
2023-12-29 20:01:34 +0000

    conversion Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score ="33"
+// let score="11abc"
+// let score=null
+// let score=undefined
+let score=true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
+
+//when you conver 33 then it will be 33
+//And when "11abc" then it will be NaN(Not a Number)
+//true=>1;false=>0
+
+//some other conversions
+// let isLoggedIn=0             //false
+// let isLoggedIn=1             //true
+// let isLoggedIn=""            //false
+let isLoggedIn="surya"          //true
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let anyNumber=33;
+let stringNumber=String(anyNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
