907919a8004f38512fed99a51094cba4eecb7b09
Suryakanth L Raibagi <114281722+SuryakanthRaibagi@users.noreply.github.com>
2024-01-15 20:06:01 +0000

    function

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,2 +1,34 @@
-//singletone
+//singleton
+//Object Literals
 
+const mySym=Symbol["key1"]//imp
+const jsUser={
+    name:"Surya",
+    [mySym]:"key1",
+    age:20,
+    location:"karnataka",
+    email:"surya@gamil.com",
+    isLoggedIn:false,
+    lastLoginDays:[
+        "Monday",
+        "Saturday"
+    ]
+}
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser[mySym]);//imp
+
+jsUser.email="suryakanth@gmail.com"
+// Object.freeze(jsUser)
+jsUser.email="raj@gmail.com"
+// console.log(jsUser.email);
+
+
+jsUser.greeting=function(){
+    console.log("Hello Js user");
+}
+jsUser.greeting2=function(){
+    console.log(`Hello Js user, ${this.name}`);
+}
+console.log(jsUser.greeting());
+console.log(jsUser.greeting2());
